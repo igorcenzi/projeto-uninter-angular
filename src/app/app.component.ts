fc505edecb0b6743f513c50cb5d66b34f86bca0f
Igor Cenzi <igor.cenzi2@gmail.com>
2022-05-22 15:13:15 -0300

    upload projeto

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,20 @@
 import { Component } from '@angular/core';
+import { lista } from './arquivo';
+
+//Cria interface da lista de alunos tipando cada atributo
+export interface IAluno {
+  nome: string;
+  ru: string;
+  nomeDoCurso: string;
+  dataNascimento: string;
+}
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
   title = 'projeto_uninter';
+  //Cria a lista para ser usada no ngfor
+  alunos: IAluno[] = lista;
 }
---
+++ src/app/arquivo.ts
@@ -0,0 +1,34 @@
+//Cria lista que seráq exibida na tabela
+
+export const lista = [
+  {
+    nome: 'Igor Cenzi',
+    ru: '3523213',
+    nomeDoCurso: 'Engenharia de Software',
+    dataNascimento: '17/12/1994',
+  },
+  {
+    nome: 'João da Silva',
+    ru: '3421531',
+    nomeDoCurso: 'Engenharia de Software',
+    dataNascimento: '21/02/1998',
+  },
+  {
+    nome: 'Netinho de Paula',
+    ru: '4315762',
+    nomeDoCurso: 'Psicologia',
+    dataNascimento: '15/08/1917',
+  },
+  {
+    nome: 'Maria de Jesus',
+    ru: '6412078',
+    nomeDoCurso: 'Engenharia de Produção',
+    dataNascimento: '04/09/2004',
+  },
+  {
+    nome: 'Joana Costa',
+    ru: '7003150',
+    nomeDoCurso: 'Engenharia Civil',
+    dataNascimento: '01/01/2000',
+  },
+];
